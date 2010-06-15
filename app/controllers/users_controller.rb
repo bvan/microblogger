5e503c3f5f01eef350a4aa59b48af23114695f30
Blake VanDrimlen <blake.van@me.com>
2010-06-15 12:58:02 -0700

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,18 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign Up"
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to microBlogger!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
